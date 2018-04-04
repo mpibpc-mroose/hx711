6d38cdaf75bc0843d9c623998d87190898074c89
Marco Roose <marco.roose@mpibpc.mpg.de>
2018-04-04 13:14:46 +0200

    switched to pipenv

--- setup.py
+++ setup.py
@@ -1,32 +1,72 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-
-"""The setup script."""
+import pathlib
+import subprocess
 
 from setuptools import setup, find_packages
+from setuptools.command.install import install
+from setuptools.command.develop import develop
 
-with open('ReadMe.md') as readme_file:
-    readme = readme_file.read()
+__requires__ = ['pipenv']
+
+packages = find_packages(exclude=['tests'])
+base_dir = pathlib.Path(__file__).parent
+
+pipenv_command = ['pipenv', 'install', '--deploy', '--system']
+pipenv_command_dev = ['pipenv', 'install', '--dev', '--deploy', '--system']
+
+
+class PostDevelopCommand(develop):
+    """Post-installation for development mode."""
+
+    def run(self):
+        subprocess.check_call(pipenv_command_dev)
+        develop.run(self)
+
+
+class PostInstallCommand(install):
+    """Post-installation for installation mode."""
+
+    def run(self):
+        subprocess.check_call(pipenv_command)
+        install.run(self)
+
+
+with open(str(base_dir / 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
 
 setup(
     name='hx711',
     version='1.1.2',
     description="A library to drive a HX711 load cell amplifier on a Raspberry Pi",
-    long_description=readme,
-    long_description_content_type="text/markdown",
+    url='https://github.com/mpibpc-mroose/hx711/',
     author="Marco Roose",
     author_email='marco.roose@gmx.de',
-    url='https://github.com/mpibpc-mroose/hx711',
-    packages=find_packages(include=['hx711']),
-    include_package_data=True,
-    license="MIT license",
-    zip_safe=False,
-    keywords='hx711',
     classifiers=[
         'Intended Audience :: Developers',
         'License :: OSI Approved :: MIT License',
         'Natural Language :: English',
         'Programming Language :: Python :: 3.5',
     ],
+    project_urls={
+        'Bug Reports': 'https://github.com/mpibpc-mroose/hx711/issues',
+    },
+    use_scm_version=True,
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+
+    packages=packages,
+    package_data={},
+    include_package_data=True,
+    license="MIT license",
+    zip_safe=False,
+    keywords='hx711',
+    setup_requires=['setuptools_scm'],
+    cmdclass={
+        'develop': PostDevelopCommand,
+        'install': PostInstallCommand,
+    },
     test_suite='tests',
 )
+
